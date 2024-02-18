4b44793d62b045ba5ce023331531ca217a0de613
kg0r0 <kg0r0@yahoo.co.jp>
2024-02-18 21:01:36 +0900

    implement webauthn.User

--- user.go
+++ user.go
@@ -1,10 +1,49 @@
 package main
 
-import "github.com/go-webauthn/webauthn/webauthn"
+import (
+	"encoding/binary"
+
+	"github.com/go-webauthn/webauthn/protocol"
+	"github.com/go-webauthn/webauthn/webauthn"
+)
 
 type User struct {
 	id          uint64
 	username    string
 	displayName string
 	credentials []webauthn.Credential
+}
+
+func (u User) WebAuthnID() []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutUvarint(buf, uint64(u.id))
+	return buf
+}
+
+func (u User) WebAuthnName() string {
+	return u.username
+}
+
+func (u User) WebAuthnDisplayName() string {
+	return u.displayName
+}
+
+func (u User) WebAuthnCredentials() []webauthn.Credential {
+	return u.credentials
+}
+
+func (u User) WebAuthnIcon() string {
+	return ""
+}
+
+func (u User) CredentialExcludeList() []protocol.CredentialDescriptor {
+	credentialExcludeList := []protocol.CredentialDescriptor{}
+	for _, cred := range u.credentials {
+		descriptor := protocol.CredentialDescriptor{
+			Type:         protocol.PublicKeyCredentialType,
+			CredentialID: cred.ID,
+		}
+		credentialExcludeList = append(credentialExcludeList, descriptor)
+	}
+	return credentialExcludeList
 }
